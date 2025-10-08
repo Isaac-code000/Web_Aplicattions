601a2605993a15b613655a332f819f87a857f0b4
isaac <isaac.cade26@gmail.com>
2025-10-08 20:56:02 -0300

    Primeira_api

---
+++ Primeira_API/Primeira_API/Controllers/PersonagemController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Primeira_API.Data;
+using Primeira_API.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace Primeira_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonagemController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+        public PersonagemController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> NewPersonagem([FromBody] Personagem personagem)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            _appDbContext.DbPersonagem.Add(personagem);
+
+            await _appDbContext.SaveChangesAsync();
+            return Ok(personagem);
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Personagem>>> GetPersonagens()
+        {
+            var personagens = await _appDbContext.DbPersonagem.ToListAsync();
+            return Ok(personagens);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Personagem>> GetPersonagem(int id)
+        {
+            Personagem personagem = await _appDbContext.DbPersonagem.FindAsync(id);
+            if (personagem == null) {
+                return NotFound("Personagem não encontrado =( ");
+            }
+            return Ok(personagem);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdatePersonagem(int id,[FromBody]Personagem atualizado)
+        {
+            var atual = await _appDbContext.DbPersonagem.FindAsync(id);
+            if (atual == null) {
+                return NotFound("O personagem nao existe");
+            }
+            _appDbContext.Entry(atual).CurrentValues.SetValues(atualizado);
+            await _appDbContext.SaveChangesAsync();
+            return StatusCode(201,atual);
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePersonagem(int id)
+        {
+           var personagem = await _appDbContext.DbPersonagem.FindAsync(id);
+           if (personagem == null) return NotFound("O personagem nao existe");
+          
+           _appDbContext.DbPersonagem.Remove(personagem);
+           await _appDbContext.SaveChangesAsync();
+           return Ok(personagem);
+        }
+    }
+}
---
+++ Primeira_API/Primeira_API/Models/Personagem.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Primeira_API.Models
+{
+    public class Personagem
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Nome é um campo obrigatorio")]
+        [MaxLength(50,ErrorMessage = "Limite de 50 caracteres")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Tipo é um campo obrigatorio")]
+        [MaxLength(50,ErrorMessage = "Limite de 50 caracteres")]
+        public string Tipo { get; set; }
+    }
+}
