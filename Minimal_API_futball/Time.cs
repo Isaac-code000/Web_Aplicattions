c3f762a4d35f7482e26cea4cb2f94ec9234f30b2
isaac <isaac.cade26@gmail.com>
2025-10-09 14:00:01 -0300

    Start

---
+++ Minimal_API_futball/Time.cs
@@ -0,0 +1,9 @@
+﻿namespace Minimal_API_futball;
+
+public class Time
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public string? Cidade { get; set; }
+    public string? Titulos { get; set; } 
+}
