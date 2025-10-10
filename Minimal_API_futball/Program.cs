93545c040787f437a7ea69ed19e307e626ce281b
isaac <isaac.cade26@gmail.com>
2025-10-09 21:08:43 -0300

    Conections

---
+++ Minimal_API_futball/Program.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using Minimal_API_futball;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+
+var app = builder.Build();
+app.MapGet("/times", async (AppDbContext db) => 
+{
+    return await db.Times.ToListAsync();
+});  
+app.MapGet("/times/{id}", async (AppDbContext db,int id) => 
+{
+    var time = await db.Times.FindAsync(id);
+    return time is null ? Results.NotFound() : Results.Ok(time);
+});
+app.MapPost("/times", async (AppDbContext db, Time time) =>
+{   
+    db.Times.Add(time);
+    await db.SaveChangesAsync();
+    return Results.Created($"O time {time.Nome} foi inserido com sucesso",time);
+});
+app.MapPut("/times/{id}", async (AppDbContext db, int id,Time atualizado) =>
+{
+    var time = await db.Times.FindAsync(id);
+    if (time is null)
+    {
+        return Results.NotFound("Time nao encontrado");
+    }
+    time.Nome = atualizado.Nome; 
+    time.Id = id;
+    time.Cidade = atualizado.Cidade;
+    time.Titulos = atualizado.Titulos;   
+   
+    await db.SaveChangesAsync();
+    return Results.Ok(time);
+});
+
+
+app.MapDelete("/times/{id}",
+    async (AppDbContext db,
+        int id) =>
+    {
+        var time = await db.Times.FindAsync(id);
+        if (time is null)
+        {
+            return Results.NotFound();
+        }
+
+        db.Times.Remove(time);
+        db.SaveChanges();
+        return Results.NoContent();
+    });
+app.Run();
+
