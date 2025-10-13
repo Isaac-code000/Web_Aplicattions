1fc3d5d9fa24b5a251fb62d312520d6ee4871146
Isaac-code000 <isaac.cade26@gmail.com>
2025-10-12 22:10:45 -0300

    Testing

--- SalesWebMVC/SalesWebMVC/Data/AppDbContext.cs
+++ SalesWebMVC/SalesWebMVC/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Models;
 
 namespace SalesWebMVC.Data;
 
@@ -9,5 +10,7 @@ public class AppDbContext : DbContext
     {
         
     }
+    
+    DbSet<Department> BdDepartment => Set<Department>();
     
 }
